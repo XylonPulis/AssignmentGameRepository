a6b3ec69c54c03133f67fba70d26e41e5e070624
XylonPulis <xylon.pulis.a100889@mcast.edu.mt>
2017-12-12 12:37:59 +0100

    -- Removed Level 1 Background -- Added Score Colliders -- Fixed Boundaries

--- FootballGame/Assets/LevelManager.cs
+++ FootballGame/Assets/LevelManager.cs
@@ -1,11 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LevelManager : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+    
+public void LoadNewScene(string sceneName)
+{
+    SceneManager.LoadScene("Level1Scene");
+}
+
+public void QuitGame()
+{
+    UnityEditor.EditorApplication.isPlaying = false;
+}
+
+// Use this for initialization
+void Start () {
 		
 	}
 	
