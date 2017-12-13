f2a9d7e9d638d09c8e2d4076275d1a787545eba8
XylonPulis <xylon.pulis.a100889@mcast.edu.mt>
2017-12-13 15:34:45 +0100

    -- Added Level 2 Obstacles -- Changed Background -- Added level 3 Obstacles -- Added movement to obstacle on lvl 3

---
+++ FootballGame/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+    
+public void LoadNewScene(string sceneName)
+{
+    SceneManager.LoadScene("Level1Scene");
+}
+
+public void QuitGame()
+{
+    UnityEditor.EditorApplication.isPlaying = false;
+}
+
+// Use this for initialization
+void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
