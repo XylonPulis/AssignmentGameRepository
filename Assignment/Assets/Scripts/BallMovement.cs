b6d478d9cf71f68e97537cdb4995abe261991ecd
XylonPulis <xylon.pulis.a100889@mcast.edu.mt>
2017-12-14 22:21:40 +0100

    -- HAD TO START ASSIGNMENT OVER -- -- Added All Scenes (Menu, Level_01, Level_02, Level 03, Play again Menu, Exit Menu) -- -- Functional play button in Main Menu -- -- Functional ball in all levels -- -- Functional boundaries -- -- Added Obstacles in Level_02 & Level_03 --

---
+++ Assignment/Assets/Scripts/BallMovement.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallMovement : MonoBehaviour {
+
+    Rigidbody2D rb;
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+
+        //Ball chooses direction to fly to
+        rb = GetComponent<Rigidbody2D>();
+
+        //Random x-axis direction
+        int xDirection = Random.Range(0, 2);
+
+        //Random y-axis direction
+        int yDirection = Random.Range(0, 2);
+
+        Vector2 launchDirection = new Vector2();
+
+        if (xDirection == 0)
+        {
+            launchDirection.x = -8f;
+        }
+
+        if (xDirection == 1)
+        {
+            launchDirection.x = 8f;
+        }
+
+        if (yDirection == 0)
+        {
+            launchDirection.y = -8f;
+        }
+
+        if (yDirection == 2)
+        {
+            launchDirection.y = 0f;
+        }
+
+        rb.velocity = launchDirection;
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
